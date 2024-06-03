c9f0972318a7ce3d5d34998cb01d527332c0a7b9
Rakshita Vijay <rakshita.vijay1@gmail.com>
2024-06-03 19:12:53 +0530

    HelloWorld renamed; java_git_notes created

--- 001_hello_world.java
+++
@@ -1,5 +0,0 @@
-public class hello_world {
-  public static void main(string args[]) {
-    System.out.print("Hello world");
-  }
-}
---
+++ HelloWorld001.java
@@ -0,0 +1,6 @@
+public class HelloWorld001 {
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+    // System.out.println("Hello world: " + args[0]);
+  }
+}
