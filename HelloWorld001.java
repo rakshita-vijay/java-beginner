d36e87d8c07f0c14c19143f4fb474000981b2793
Rakshita Vijay <rakshita.vijay1@gmail.com>
2024-06-03 19:59:54 +0530

    renamed java_notes & readme; working HelloWorld code, 002_var_def file created

--- HelloWorld001.java
+++ HelloWorld001.java
@@ -1,6 +1,14 @@
 public class HelloWorld001 {
   public static void main(String[] args) {
     System.out.println("Hello world");
-    // System.out.println("Hello world: " + args[0]);
+    System.out.println("Hello world: " + args[0]);
+    System.out.println("Hello world: " + args[1]);
+    System.out.println("Hello world: " + args[0] + args[1]);
+    System.out.println("Hello world: " + args[0] + " " + args[1]);
+
+    System.out.println("");
+
+    System.out.print("Hello ");
+    System.out.println("world");
   }
 }
---
+++ VariableDefinitions002.java
@@ -0,0 +1,5 @@
+public class VariableDefinitions002 {
+  public static void main(String[] args) {
+    int myVariable = 5;
+  }
+}
