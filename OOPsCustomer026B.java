48a519023e1a7ba9578dbea53d8994ab79ad0847
Rakshita Vijay <rakshita.vijay1@gmail.com>
2024-06-18 21:03:59 +0530

    026 - overloaded constructors, working code

---
+++ OOPsCustomer026B.java
@@ -0,0 +1,27 @@
+public class OOPsCustomer026B {
+  private String name = "ABC";
+  private double creditLimit = 12345;
+  private String emailAddress = "ABC@gmail.com";
+
+  public String getName() { return name; }
+  public double getCreditLimit() { return creditLimit; }
+  public String getEmailAddress() { return emailAddress; }
+
+  public OOPsCustomer026B() { }
+
+  public OOPsCustomer026B(String name, double creditLimit, String emailAddress) {
+    this.name = name;
+    this.creditLimit = creditLimit;
+    this.emailAddress = emailAddress;
+  }
+
+  public OOPsCustomer026B(String name, String emailAddress) {
+    this(name, 13579, emailAddress);
+  }
+
+  public void printDetails() {
+    System.out.println("Name: " + name);
+    System.out.println("Credit Limit: " + creditLimit);
+    System.out.println("Email Address: " + emailAddress);
+  }
+}
