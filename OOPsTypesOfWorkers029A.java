a751bb205e9b8091e1e6442cd459063d29455e10
Rakshita Vijay <rakshita.vijay1@gmail.com>
2024-06-28 09:19:10 +0530

    minor changes made

--- OOPsTypesOfWorkers029A.java
+++ OOPsTypesOfWorkers029A.java
@@ -4,9 +4,17 @@ public class OOPsTypesOfWorkers029A {
     System.out.println(max);
 
     System.out.println();
-    OOPsEmployee029C tim = new OOPsEmployee029C("Tim", "11/11/1985", 77001, "01/01/2020");
+    OOPsEmployee029C brit = new OOPsEmployee029C("Brit", "11/11/1980", 77001, "01/01/2020");
+    System.out.println(brit);
+    System.out.println("Age: " + brit.getAge());
+    System.out.println("Pay: " + brit.collectPay());
+
+    System.out.println();
+    OOPsEmployee029C tim = new OOPsEmployee029C("Tim", "11/11/1985", "02/02/2020");
     System.out.println(tim);
-    System.out.println("Age: " + tim.getAge());
-    System.out.println("Pay: " + tim.collectPay());
+
+    System.out.println();
+    OOPsEmployee029C joe = new OOPsEmployee029C("Joe", "11/11/1990", "03/03/2020");
+    System.out.println(joe);
   }
 }
